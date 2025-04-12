4bacd2f06ba72156b0754ab805800211eea66d4e
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-12 04:22:14 +0200

    added the eventhandler function to actually get the changes made by the user inside the state of the component

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,12 +2,20 @@
 
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    console.log(value);
+    setCode(value);
+  };
+
   return (
     <div>
       <Editor
@@ -16,6 +24,7 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         language="JavaScript"
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </div>
   );
