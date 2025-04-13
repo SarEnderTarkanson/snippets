564f7d76d617a735e365c562ba2520bf907c1a01
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-13 02:41:03 +0200

    defined the logic inside the server action and called that action from a client component

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -17,6 +17,12 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
     setCode(value);
   };
 
+  const editSnippetAction = actions.editSnippet.bind(
+    null,
+    snippet.id,
+    code
+  );
+
   return (
     <div>
       <Editor
@@ -27,6 +33,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
